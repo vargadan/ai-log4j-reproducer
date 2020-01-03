7ec1f736fcd2fcbb3b0e3c4820cbc68c214533a5
Daniel Varga <danielvrg@icloud.com>
2020-01-03 14:18:22 +0100

    dynamic response lookup in git

--- src/main/java/MockServerRunner.java
+++ src/main/java/MockServerRunner.java
@@ -4,8 +4,6 @@ import org.mockserver.model.HttpResponse;
 
 import java.net.URI;
 import java.time.Duration;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class MockServerRunner {
 
@@ -13,22 +11,26 @@ public class MockServerRunner {
 
     final static String BRANCH = "master";
 
-    static String getGitContent(String repo, String branch, String file) throws Exception {
+    static HttpResponse respondGitContent(String repo, String branch, String file) throws Exception {
         var gitRequest = java.net.http.HttpRequest.newBuilder()
                 .uri(new URI("https://raw.githubusercontent.com/vargadan/" + repo + "/" + branch + "/" + file))
                 .timeout(Duration.ofSeconds(10))
                 .GET().build();
         var gitResponse = java.net.http.HttpClient.newBuilder().build()
                 .send(gitRequest, java.net.http.HttpResponse.BodyHandlers.ofString());
-        var gitResponseBody = gitResponse.body();
-        return gitResponseBody;
+        if (gitResponse.statusCode() == 404) {
+            return HttpResponse.notFoundResponse();
+        } else {
+            return HttpResponse.response()
+                    .withStatusCode(gitResponse.statusCode())
+                    .withBody(gitResponse.body());
+        }
     }
 
     public static void main(String... args) throws Exception {
         var mockServer = ClientAndServer.startClientAndServer(1080);
-        mockServer.when(HttpRequest.request().withMethod("GET").withPath("/api/ui")
-        ).respond(
-                HttpResponse.response(getGitContent(GIT_REPOSITORY, BRANCH,  "/api/ui/response.json")));
+        mockServer.when(HttpRequest.request().withMethod("GET").withPath("/api/.*")
+        ).respond(httpRequest -> respondGitContent(GIT_REPOSITORY, BRANCH, httpRequest.getPath() + "/response.json"));
     }
 }
 
