ed739d8e6b29d52d4eee604114316d1346d6ac1a
Daniel Varga <danielvrg@icloud.com>
2020-07-23 11:17:11 +0200

    add dummy test

---
+++ src/test/java/com/gocompliant/smarthub/mockserver/DummyTest1.java
@@ -0,0 +1,12 @@
+package com.gocompliant.smarthub.mockserver;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest1 {
+
+    @Test
+    public void succeed() {
+        // always
+    }
+}
